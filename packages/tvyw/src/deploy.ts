0e197de83abdbd44085a6f47fff98c7af48b80be
inbegroad <azmi.abas.989@gmail.com>
2022-07-26 13:25:51 +0200

    fixing deploy script bug

--- packages/tvyw/src/deploy.ts
+++ packages/tvyw/src/deploy.ts
@@ -1,25 +1,42 @@
-import yarn from "@yarnpkg/shell";
-import { questionsMonoList } from "./tools/questions";
+import { execute } from "@yarnpkg/shell";
+import { existsSync, mkdirSync } from "fs-extra";
+import { join } from "path";
 import { readPackageJson } from "./tools/read";
 import { resolveConfig } from "./tools/resolveProjMan";
+import { getVersionConfig } from "./tools/version-config";
 import { writePackageJson } from "./tools/write";
 
 export const deploy = async () => {
   const projMan = resolveConfig();
   if (projMan.repoType === "monoRepo" && projMan.root) {
-    await yarn.execute(`turbo run deploy`);
+    await execute(`turbo run deploy`);
   } else {
     if (projMan.workspaceType === "app")
       throw new Error("You can't deploy an app");
-    const pkgJson = await readPackageJson();
-    const version = await questionsMonoList.version(
-      pkgJson.name,
-      pkgJson.version
-    );
-    pkgJson.version = version;
-    await writePackageJson(pkgJson);
-    console.log("Deploying...");
-
-    // await yarn.execute("npm publish");
+    await dep();
   }
 };
+
+async function dep() {
+  const versionJson = getVersionConfig();
+  const pkgJson = await readPackageJson();
+
+  const cachedPath = join(
+    process.cwd(),
+    "..",
+    "..",
+    "node_modules",
+    `.cache/${versionJson.name}/${pkgJson.name}`
+  );
+
+  if (!existsSync(cachedPath)) mkdirSync(cachedPath, { recursive: true });
+  const cachedFilePath = join(cachedPath, `${pkgJson.name}.temp.json`);
+
+  await writePackageJson(pkgJson, cachedFilePath);
+  delete pkgJson.scripts;
+  delete pkgJson.devDependencies;
+  await writePackageJson(pkgJson);
+  await execute("npm publish");
+  const temp = await readPackageJson(cachedFilePath);
+  await writePackageJson(temp, cachedFilePath);
+}
